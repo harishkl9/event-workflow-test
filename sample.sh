c78b3ae43867d5576a96a49405cd144b27377d8c
harishkl9 <101081474+harishkl9@users.noreply.github.com>
2024-03-28 14:46:31 +0530

    Update sample.sh

--- sample.sh
+++ sample.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-logit() {
-  severity=${1:-INFO}
-  msg=$2
-  >&2 echo "$severity $(date -u +'%Y-%m-%d-%H:%M:%S') $msg"
-}
+
 
 call_l9iac(){
  
-    stats="total_actions=25 failed_actions=5 reconcile=3 total_create_update_delete=4 create=10 update=5 delete=6"    
-    logit "INFO" "$stats"
+    tatus_log_file="status.log"
+    stats="total_actions=25 failed_actions=5 reconcile=3 total_create_update_delete=4 create=10 update=5 delete=6" 
+    echo "INFO" "$stats" >> "$status_log_file"
 }
 
 
