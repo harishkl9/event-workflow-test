7852b6549ce12a8ed496e1d569b65f02a10c69a8
harishkl9 <101081474+harishkl9@users.noreply.github.com>
2024-03-28 10:15:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     
     logging.info("testing just github workflow working one this is script one or job one")
 
-    logging.info({
+    print({
         "total_count":30,
         "total_failure": 10,
         "total_sucess": 10,
